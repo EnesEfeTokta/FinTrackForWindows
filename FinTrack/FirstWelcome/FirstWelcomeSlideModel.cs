c912d73056e58f55151011192ff147d2f47df2b9
Enes Efe Tokta <enesefetokta009@gmail.com>
2025-03-26 12:44:52 +0300

    An application presentation slide was created.

--- FinTrack/FirstWelcome/FirstWelcomeSlideModel.cs
+++ FinTrack/FirstWelcome/FirstWelcomeSlideModel.cs
@@ -12,18 +12,8 @@ namespace FinTrack.FirstWelcome
 {
     public class FirstWelcomeSlideModel
     {
-        public List<SlidePage> slides { get; set; } = new List<SlidePage>();
-
-        private void SlideCreate()
-        {
-            slides.Add(new SlidePage { title = string.Empty, content = string.Empty, image = string.Empty });
-        }
-    }
-
-    public class SlidePage
-    {
-        public string title { get; set; } = string.Empty;
-        public string content { get; set; } = string.Empty;
-        public string image { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
+        public string Header { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
     }
 }
